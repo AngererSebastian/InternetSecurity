3fe147654742cab8b3deb5cd6640682217858f41
Detta Alessandro <52331383+1BHIF@users.noreply.github.com>
2020-04-21 18:39:00 +0200

    Update encrypt.js

--- ClientScript/encrypt.js
+++ ClientScript/encrypt.js
@@ -10,15 +10,17 @@ function encrypt (public, product, message)
 	return bigInt(message).modPow(public, product);	
 }
 function getContent(){
-   var x = document.getElementById("test").value;// test is the ID of a text area
+   var x = document.getElementById("test").value;
    console.log(x);
    convert(x);
 }
 function convert(x){
-   var out;
+   var out = [];
    console.log(x);
-   console.log(x.length);
    for(var i = 0; i< x.length; i++){
-      console.log(x[i].charCodeAt(0)); // 0 is the index of the string
+      out.push(x.charCodeAt(i));
    }
-}
+   for(var i = 0; i< x.length; i++){
+      console.log(out[i]);
+   }
+}}
