7e06513c570ddf08713aea6b1d883c300173522d
Detta Alessandro <52331383+1BHIF@users.noreply.github.com>
2020-04-21 18:18:44 +0200

    Update encrypt.js
    Convert char in ascii number and read textarea

--- ClientScript/encrypt.js
+++ ClientScript/encrypt.js
@@ -9,3 +9,16 @@ function encrypt (public, product, message)
 {
 	return bigInt(message).modPow(public, product);	
 }
+function getContent(){
+   var x = document.getElementById("test").value;// test is the ID of a text area
+   console.log(x);
+   convert(x);
+}
+function convert(x){
+   var out;
+   console.log(x);
+   console.log(x.length);
+   for(var i = 0; i< x.length; i++){
+      console.log(x[i].charCodeAt(0)); // 0 is the index of the string
+   }
+}
