af7d616529bc666db680be44af6d6c4d73eb28d8
Detta Alessandro <52331383+1BHIF@users.noreply.github.com>
2020-04-22 10:19:31 +0200

    Update accounts.js
    Works now

--- ClientScript/accounts.js
+++ ClientScript/accounts.js
@@ -1,45 +1,85 @@
-function createAccount(){
-    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    var theUrl = "http://localhost:3000/accounts";
-        
+function createAccount(){  
+    response = 0;    
     var name = document.getElementById("fName").value;
     var pass = document.getElementById("fPassword").value;
-    if(checkIfUsernameIsUsed(name) == 0)
-    {
-        xmlhttp.open("POST", theUrl);
-        xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        xmlhttp.send(JSON.stringify({
-        "name": name,
-        "password": pass,}));
-    }
+    checkIfUsernameIsUsed(name);
 }
 function checkIfUsernameIsUsed(username)
 {
-    let response = 0;
+    var response = 0;
     var request = new XMLHttpRequest();
     request.open('GET', 'http://localhost:3000/accounts', true);
     request.onload = function () {
-
         // Begin accessing JSON data here
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
             data.forEach(account => {
                 if(account.name == username){
-                    console.log(1);
-                    response = 1;
-                }  
-            });
-            if(response != 1)
-            {
-                console.log(0);
-                response = 0;
-            }
+                response = 1;
+            }  
+        });
         } else {
             const errorMessage = document.createElement('marquee');
-            errorMessage.textContent = `Gah, it's not working!`;
-            app.appendChild(errorMessage);
+            errorMessage.textContent = `Gah, it's not working!`;                app.appendChild(errorMessage);
+        }
+        if(response == 0)
+        {
+            SendAccount();
         }
     }
     request.send();
-    return response;
+}
+function SendAccount()
+{
+    var name = document.getElementById("fName").value;
+    var pass = document.getElementById("fPassword").value;
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    var theUrl = "http://localhost:3000/accounts";
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+    "name": name,
+    "password": pass,}));
+}function createAccount(){  
+    response = 0;    
+    var name = document.getElementById("fName").value;
+    var pass = document.getElementById("fPassword").value;
+    checkIfUsernameIsUsed(name);
+}
+function checkIfUsernameIsUsed(username)
+{
+    var response = 0;
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/accounts', true);
+    request.onload = function () {
+        // Begin accessing JSON data here
+        var data = JSON.parse(this.response);
+        if (request.status >= 200 && request.status < 400) {
+            data.forEach(account => {
+                if(account.name == username){
+                response = 1;
+            }  
+        });
+        } else {
+            const errorMessage = document.createElement('marquee');
+            errorMessage.textContent = `Gah, it's not working!`;                app.appendChild(errorMessage);
+        }
+        if(response == 0)
+        {
+            SendAccount();
+        }
     }
+    request.send();
+}
+function SendAccount()
+{
+    var name = document.getElementById("fName").value;
+    var pass = document.getElementById("fPassword").value;
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    var theUrl = "http://localhost:3000/accounts";
+    xmlhttp.open("POST", theUrl);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send(JSON.stringify({
+    "name": name,
+    "password": pass,}));
+}
