b4950faac5d99c4e9c7d04bda07701eed9564857
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2020-04-28 20:43:21 +0200

    added basic functions

--- httpServer/main.go
+++ httpServer/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	//"net/url"
-	//"encoding/json"
-	//"io/ioutil"
+	"encoding/json"
+	"io/ioutil"
 	//"os"
 )
+
+type Request struct {
+	Command string
+	Argument string
+	Content string
+}
 
 var messages map[string] string = make(map[string] string)
 
@@ -21,7 +26,25 @@ func HandleRequest (w http.ResponseWriter, request *http.Request) {
 		name := request.URL.RequestURI()[1:]
 		fmt.Fprintln(w, messages[name])
 	case "POST":
+		buffer, err := ioutil.ReadAll(request.Body)
+
+		var ree Request
+		err = json.Unmarshal(buffer, ree)
+
+		if err != nil {
+			fmt.Fprintln(w, "Json data wasn't in the right format")
+		}
+
 	default:
 		fmt.Fprintln(w, "Server doesn't support this request")
+	}
+}
+
+func HandleCommands (request Request, w http.ResponseWriter) {
+	switch request.Command {
+	case "USER":
+		messages[request.Argument] = ""
+	case "SEND":
+		messages[request.Argument] = request.Content
 	}
 }
