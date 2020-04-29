98affd13166d529e86d86184bb14b81f8fe8c50d
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2020-04-29 12:29:24 +0200

    added post support

--- httpServer/main.go
+++ httpServer/main.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"encoding/json"
 	"io/ioutil"
-	//"os"
+	"os"
 )
 
 type Request struct {
@@ -18,13 +18,20 @@ var messages map[string] string = make(map[string] string)
 
 func main () {
 	http.HandleFunc("/", HandleRequest)
+
+	err := http.ListenAndServe(":" + os.Args[1], nil)
+
+	if err != nil {
+		fmt.Println("Couldn't listen on port", os.Args[1])
+	}
 }
 
 func HandleRequest (w http.ResponseWriter, request *http.Request) {
+	path := request.URL.RequestURI()[1:]
+
 	switch request.Method {
 	case "GET":
-		name := request.URL.RequestURI()[1:]
-		fmt.Fprintln(w, messages[name])
+		fmt.Fprintln(w, messages[path])
 	case "POST":
 		buffer, err := ioutil.ReadAll(request.Body)
 
@@ -35,16 +42,13 @@ func HandleRequest (w http.ResponseWriter, request *http.Request) {
 			fmt.Fprintln(w, "Json data wasn't in the right format")
 		}
 
+		if path == "" {
+			messages[ree.Argument] = ""
+		} else {
+			messages[path] = ree.Content
+		}
+
 	default:
 		fmt.Fprintln(w, "Server doesn't support this request")
-	}
-}
-
-func HandleCommands (request Request, w http.ResponseWriter) {
-	switch request.Command {
-	case "USER":
-		messages[request.Argument] = ""
-	case "SEND":
-		messages[request.Argument] = request.Content
 	}
 }
