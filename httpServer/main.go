a6b5cd9dc173ff45a9209fdb17fb73c0c3c5a0a8
AngererSebastian <angerer.sebastian.merlin@gmail.com>
2020-05-05 17:44:13 +0200

    fixed the server

--- httpServer/main.go
+++ httpServer/main.go
@@ -9,46 +9,57 @@ import (
 )
 
 type Request struct {
-	Command string
 	Argument string
 	Content string
 }
 
+type MessageHandler struct {}
+
 var messages map[string] string = make(map[string] string)
 
 func main () {
-	http.HandleFunc("/", HandleRequest)
-
-	err := http.ListenAndServe(":" + os.Args[1], nil)
+	err := http.ListenAndServe(":" + os.Args[1], new(MessageHandler))
 
 	if err != nil {
 		fmt.Println("Couldn't listen on port", os.Args[1])
 	}
 }
 
-func HandleRequest (w http.ResponseWriter, request *http.Request) {
-	path := request.URL.RequestURI()[1:]
+func (h MessageHandler) ServeHTTP (w http.ResponseWriter, request *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	path := request.URL.Path[1:]
+	fmt.Println(path)
 
 	switch request.Method {
+	//gets the messages of a user
 	case "GET":
+		fmt.Println("Retrieving")
 		if !Userexists(path) { return }
 		fmt.Fprintln(w, messages[path])
-	case "POST":
-		buffer, err := ioutil.ReadAll(request.Body)
+		fmt.Println(messages[path])
 
+	case "POST":
+
+		buffer, err := ioutil.ReadAll(request.Body)
+		fmt.Println(string(buffer))
+
+		//get request data from json
 		var ree Request
-		err = json.Unmarshal(buffer, ree)
+		err = json.Unmarshal(buffer, &ree)
 
 		if err != nil {
 			fmt.Fprintln(w, "Json data wasn't in the right format")
 		}
 
-		if path == "" {
-			messages[ree.Argument] = ""
-		} else {
-
+		if path != "" {
+			fmt.Println("Sending message")
 			if !Userexists(path) { return }
-			messages[path] = ree.Content
+			messages[path] += ree.Content
+		} else {
+			fmt.Println("Creating User")
+			if Userexists(ree.Argument) { return }
+			messages[ree.Argument] = ""
 		}
 
 	default:
